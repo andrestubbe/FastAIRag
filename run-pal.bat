4b14a4bae7c392b0a20f0dc369bfc51214244b00
Andre Stubbe <andrestubbe@proton.me>
2026-07-11 04:12:16 +0200

    Allow dynamic mainClass overrides from Batch scripts

--- run-pal.bat
+++ run-pal.bat
@@ -1,4 +1,4 @@
 @echo off
 cd examples/Pal
-call mvn -q compile exec:java -D"exec.args"="%*" -D"java.library.path"=..\..\..\FastAIVectorDB\build
+call mvn -q compile exec:java -D"exec.mainClass"="pal.PalMain" -D"exec.args"="%*" -D"java.library.path"=..\..\..\FastAIVectorDB\build
 cd ../..
